28b8fc4a64d045a1c86438377487e95f87079d96
nemanja <zfox905@gmail.com>
2021-02-05 21:07:10 +0100

    commit3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 function App() {
   return (
-
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1
+          Learn React
         </a>
       </header>
     </div>
